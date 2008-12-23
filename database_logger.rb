9524988430d91c70cf63662bca11974b9d5eeb69
Thorsten Böttger <thorsten.boettger@pc-tboettger.local>
2008-12-23 11:30:19 +0100

    disabled database access

--- database_logger.rb
+++ database_logger.rb
@@ -12,13 +12,13 @@ class DatabaseLogger
   
   def build_finished(build)
     return if @host.empty? || @username.empty? || @database.empty?
-    DBI.connect("DBI:#{@adapter}:database=#{@database};host=#{@host}", @username, @password || '') do |dbh|
+    # DBI.connect("DBI:#{@adapter}:database=#{@database};host=#{@host}", @username, @password || '') do |dbh|
       revision, build_number = build.label.split('.')
       insert_sql = "INSERT INTO builds (project,svn_revision,build_number,success,duration,start) 
                     VALUES (#{build.project.name},#{revision},#{build_number},#{build.failed? ? '0' : '1'},#{build.elapsed_time},#{build.time})"
       # dbh.do(insert_sql)
       puts insert_sql
-    end
+    # end
   end
   
   
